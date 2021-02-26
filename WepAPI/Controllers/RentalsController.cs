1ca07e85fdc74ca774682adbe56809140a4927f2
Uğur Türk <u9urturk@gmail.com>
2021-02-26 05:39:24 +0300

    WepAPI ; Inject

---
+++ WepAPI/Controllers/RentalsController.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        IRentalService _rentalService;
+
+        public RentalsController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpPost("rentcar")]
+        public IActionResult RentCar(Rental rental)
+        {
+            var result = _rentalService.RentCar(rental);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("returnedcar")]
+        public IActionResult RentedCar(Rental rental)
+        {
+            var result = _rentalService.ReturnedCar(rental);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getrentaldetails")]
+        public IActionResult GetRentalsDetails()
+        {
+            var result = _rentalService.GetRentalDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
